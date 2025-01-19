1e109226d87535c5a19a3328ea563759e3f271d6
Mohamedkl1235 <mouhamedklila45@gmail.com>
2025-01-19 20:31:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('button');
+    button.addEventListener('click', function () {
+        alert('Merci de nous contacter. Nous vous répondrons dans les plus brefs délais.');
+    });
+});
